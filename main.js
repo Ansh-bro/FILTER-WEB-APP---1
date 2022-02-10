5d97af7efe8da93e07b42fd58df433286398e441
Ansh-bro <87030975+Ansh-bro@users.noreply.github.com>
2022-02-10 18:40:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function draw(){
+  
+}
+
+function setup(){
+  canvas = createCanvas(380,300);
+  canvas.center();
+}
+
+function takeSnapshot(){
+    save('MyFilterImage.png');
+}
